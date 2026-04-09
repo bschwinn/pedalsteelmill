6a992d4667084f2c27788b7c926c3a1767fbfda0
bschwinn <bschwinn7@gmail.com>
2026-04-09 16:52:05 -0400

    refine text colors and help/about is now passable for a draft

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,9 +1,22 @@
 export const About = () => {
-
   return (
-    <div>
-      <img src="logo-full.svg" />
-      <div>about the app...</div>
+    <div className="about">
+      <div className="about-logo">
+        <img src="logo-full.svg" />
+      </div>
+      <div className="about-content">
+        <p>
+          The app is designed to show the different fret positions of the same
+          chord as well as let you craft chord charts for songs and/or passages
+          you need a quick reference for.  It is intende primarily for beginner 
+          pedal steelers and was written by a 6 string guitarist needing a handy
+          yet interactive chord reference for this strange and wonderfully complicated
+          instrument.
+        </p>
+        <p>
+          Brought to you by Stingray Engineering
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/components/help.tsx
+++ src/components/help.tsx
@@ -1,10 +1,17 @@
 export const Help = () => {
 
   return (
-    <div>
-      <div>some helpful content about the - top section</div>
-      <div>some helpful content about the - bottom section</div>
-      <div>perhaps even some open source contribution enticement !!!!</div>
+    <div className="help">
+      <p>
+        The application is comprised of two sections, each of which look similar 
+        but have different uses. The Chord Positions section allows you to see the same
+        chord in different positions up and down the neck, while the Chord Chart section 
+        allows you to compose a chord change chart.
+      </p>
+      <h4>Chord Positions</h4>
+      <p>Use the selectors to show the different fret positions of any chord, major or minor, up and down the fret board.</p>
+      <h4>Chord Chart</h4>
+      <p>Quickly comprise a chord progression by adding chord-positions to the list.  You can drag chords to re-arrange their order as well as change a chords position or major/minor.</p>
     </div>
   )
 }
